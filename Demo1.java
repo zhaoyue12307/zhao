b18d4d656cea135e9c07feb5feecba41d9d07eb5
zhaoyue <1950247346@qq.com>
2021-06-02 21:00:59 +0800

    提交java代码

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+package com.zhao.pojo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+    }
+
+public void test(){
+
+}
+public void test1(){}
+}
